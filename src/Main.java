1498539cb9c09b85d285fa91e6359de3a62b6c17
abdullinru <abdullin17021989@gmail.com>
2022-06-30 13:21:18 +0600

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        task1(nums);
+    }
+    public static void task1(List<Integer> nums) {
+        for (Integer num : nums) {
+            if (num % 2 == 1) {
+                System.out.print(num + " ");
+            }
+
+        }
     }
 }
+
