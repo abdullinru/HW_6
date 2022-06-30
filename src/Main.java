a840d19e2bb9076a05173c33da957f2d0dd3c9cb
abdullinru <abdullin17021989@gmail.com>
2022-06-30 13:33:28 +0600

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,22 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<String> str = new ArrayList<>();
+        str.add("папа");
+        str.add("мама");
+        str.add("баба");
+        str.add("дедушка");
+        str.add("папа");
+        str.add("мама");
+        str.add("баба");
+        str.add("дедушка");
+        str.add("сестра");
+        str.add("сын");
+        str.add("дочь");
+
 //        task1(nums);
-        task2(nums);
+//        task2(nums);
+        task3(str);
 
     }
     public static void task1(List<Integer> nums) {
@@ -27,6 +41,13 @@ public class Main {
             }
         }
         System.out.println(numSet);
+    }
+    public static void task3(List<String> str) {
+        Set<String> str1 = new HashSet<>();
+        for (String s : str) {
+            str1.add(s);
+        }
+        System.out.println(str1);
     }
 }
 
